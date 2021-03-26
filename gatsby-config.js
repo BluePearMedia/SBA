d7a9e13dfef8a86f6842df2a63319369c74352a0
HeyImGary <gary.stuart.james@gmail.com>
2021-03-26 16:36:44 +0000

    adding advance site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: "acc",
-    siteUrl: "https://flamboyant-jones-ebac02.netlify.app",
+    title: 'acc',
+    siteUrl: 'https://flamboyant-jones-ebac02.netlify.app',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-material-ui",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-material-ui',
+    'gatsby-plugin-advanced-sitemap',
   ],
 };
